db1ab314776012ea517ed35205308a300cf0f5d8
Alex Dresko <me@alexdresko.com>
2016-07-28 02:22:48 +0000

    change of gulp plans

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    rename = require('gulp-rename');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
 gulp.task('build', function() {
-  gulp.src('dev/**')
-  
+  gulp.src('**/*-dev.*')
+  .pipe(rename(function (path) {
+    path.basename = path.basename.replace(/-dev/, '');
+  }))
   .pipe(gulp.dest(''));
 });
